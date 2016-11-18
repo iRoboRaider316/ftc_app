705afa30356528374cb96054f3696abd5e74ae0e
RedCater <RedCater360@yahoo.com>
2016-11-17 18:12:54 -0600

    New empty crossfire autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cf_auto_red.java
@@ -0,0 +1,160 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+/**
+ * Created by James on 11/17/2016.
+ */
+public class cf_auto_red {
+    Servo hopper;
+    DcMotor catapult;
+    DcMotor sweeper;
+    DcMotor lDrive1;
+    DcMotor lDrive2;
+    DcMotor rDrive1;
+    DcMotor rDrive2;
+    GyroSensor gyro;
+    TouchSensor touch;
+    DistanceSensor range;
+    OpticalDistanceSensor OD;
+
+    public void Launch() throws InterruptedException {
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
