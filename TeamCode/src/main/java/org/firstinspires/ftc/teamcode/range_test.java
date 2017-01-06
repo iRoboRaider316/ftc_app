ccef6e9d3a0ab5e9b32499266b1642b6229826fd
RedCater <RedCater360@yahoo.com>
2017-01-05 20:06:09 -0600

    Did i mess up Kunt?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/range_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/range_test.java
@@ -14,6 +14,7 @@ Support is available by emailing support@modernroboticsinc.com.
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -23,21 +24,31 @@ import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "Range Linear", group = "MRI")
-@Disabled
+@TeleOp(name = "Range2 Linear", group = "MRI")
+
 public class range_test extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
     byte[] range1Cache; //The read will return an array of bytes. They are stored in this variable
 
-    I2cAddr RANGE1ADDRESS = new I2cAddr(0x14); //Default I2C address for MR Range (7-bit)
-    public static final int RANGE1_REG_START = 0x04; //Register to start reading
-    public static final int RANGE1_READ_LENGTH = 2; //Number of byte to read
+    I2cAddr RANGE1ADDRESS = new I2cAddr(0x28); //Default I2C address for MR Range (7-bit)
+    I2cAddr RANGE2ADDRESS = new I2cAddr(0x18);
+
+
+    //private I2cAddr RANGE1ADDRESS = new I2cAddr(0x28); //Default I2C address for MR Range (7-bit)
+    private static final int RANGE1_REG_START = 0x28; //Register to start reading
+    private static final int RANGE1_READ_LENGTH = 2; //Number of byte to read
+    private ModernRoboticsI2cRangeSensor rangeSensor;
+    //2nd range
+    private static final int RANGE2_REG_START = 0x18; //Register to start reading
+    private static final int RANGE2_READ_LENGTH = 2; //Number of byte to read
+    private ModernRoboticsI2cRangeSensor rangeSensor2;
 
     public I2cDevice RANGE1;
     public I2cDeviceSynch RANGE1Reader;
-
+    public I2cDevice RANGE2;
+    public I2cDeviceSynch RANGE2Reader;
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -47,15 +58,28 @@ public class range_test extends LinearOpMode {
         RANGE1 = hardwareMap.i2cDevice.get("range");
         RANGE1Reader = new I2cDeviceSynchImpl(RANGE1, RANGE1ADDRESS, false);
         RANGE1Reader.engage();
+        I2cDevice RANGE1 = hardwareMap.i2cDevice.get("range");
+        I2cDeviceSynch RANGE1Reader = new I2cDeviceSynchImpl(RANGE1, RANGE1ADDRESS, false);
+
+
+        RANGE2 = hardwareMap.i2cDevice.get("range");
+        RANGE2Reader = new I2cDeviceSynchImpl(RANGE2, RANGE2ADDRESS, false);
+        RANGE2Reader.engage();
+        I2cDevice RANGE2 = hardwareMap.i2cDevice.get("range2");
+        I2cDeviceSynch RANGE2Reader = new I2cDeviceSynchImpl(RANGE2, RANGE2ADDRESS, false);
+        RANGE1Reader.engage();
 
         waitForStart();
         runtime.reset();
 
         while (opModeIsActive()) {
-            range1Cache = RANGE1Reader.read(RANGE1_REG_START, RANGE1_READ_LENGTH);
+            byte[] range1Cache = RANGE1Reader.read(RANGE1_REG_START, RANGE1_READ_LENGTH);
+            byte[] range2Cache = RANGE2Reader.read(RANGE2_REG_START, RANGE2_READ_LENGTH);
 
             telemetry.addData("Ultra Sonic", range1Cache[0] & 0xFF);
             telemetry.addData("ODS", range1Cache[1] & 0xFF);
+            telemetry.addData("Ultra Sonic2", range2Cache[0] & 0xFF);
+            telemetry.addData("ODS2", range2Cache[1] & 0xFF);
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
 
