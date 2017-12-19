d2af134888b811a88b7fcee951e1e866d558565a
Jason Rahm <citizenelah@gmail.com>
2017-12-19 13:26:06 -0600

    Finalized aftershock_teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aftershock_teleop.java
@@ -0,0 +1,167 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="aftershock_teleop", group="TeleOp")
+@Disabled
+public class aftershock_teleop extends OpMode{
+
+    public DcMotor lfDriveM, lbDriveM, rfDriveM, rbDriveM, glyphLiftM;
+    public Servo lGlyphS, rGlyphS, jewelExtendS, jewelKnockS;
+
+    int power = 1;
+
+    boolean halfSpeed = false;
+    boolean fullSpeed = false;
+
+    double floorLeft;
+    double floorRight;
+
+    private double getDirection(double inputPower) {
+        if(inputPower == 0) {
+            return 1;
+        } else {
+            return inputPower * Math.abs(inputPower);
+        }
+    }
+
+    int drive = 0;
+
+
+
+    public void init() {
+        lfDriveM = hardwareMap.dcMotor.get("lfDriveM");
+        lbDriveM = hardwareMap.dcMotor.get("lbDriveM");
+        rfDriveM = hardwareMap.dcMotor.get("rfDriveM");
+        rbDriveM = hardwareMap.dcMotor.get("rbDriveM");
+
+        glyphLiftM = hardwareMap.dcMotor.get("glyphLiftM");
+
+        lGlyphS = hardwareMap.servo.get("lGlyphS");
+        rGlyphS = hardwareMap.servo.get("rGlyphS");
+
+
+//        jewelExtendS = hardwareMap.servo.get("jewelExtendS");
+//        jewelKnockS = hardwareMap.servo.get("jewelKnockS");
+
+
+        rfDriveM.setDirection(DcMotor.Direction.REVERSE);
+        rbDriveM.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void loop() {
+        float rStick1 = gamepad1.right_stick_y;
+        float lStick1 = gamepad1.left_stick_y;
+
+        boolean lBumper1 = gamepad1.left_bumper;
+        boolean rBumper1 = gamepad1.right_bumper;
+
+        double leftPower;
+        double rightPower;
+        double speed;
+
+        ///DRIVER CODE\\\
+
+
+        // Universal drive train power switch case
+        if (rBumper1 && power == 1) {
+            halfSpeed = true;
+        } else if (halfSpeed && !rBumper1) {
+            power = 2;
+            halfSpeed = false;
+        } else if (rBumper1 && power == 2) {
+            fullSpeed = true;
+        } else if (fullSpeed && !rBumper1) {
+            power = 1;
+            fullSpeed = false;
+        }
+
+        switch (power) {
+            case 1:
+                // divide all powers by 1 (full speed)
+                speed = 1;
+                power = 1;
+                break;
+            case 2:
+                // divide all powers by 2 (half speed)
+                speed = 2;
+                power = 2;
+                break;
+            default:
+                speed = 1;
+                break;
+        }
+
+        // This sets the power of the drive motors to based on the joystick position using an Exponential Scale Algorithm
+        if (gamepad1.a) { // Backward 100%
+            drive = 1;
+        }
+        if (gamepad1.y) { //Forward 100%
+            drive = 2;
+        }
+        if (rBumper1) { //1/2 speed
+            speed = 2;
+        }
+        if (lBumper1) { //full speed
+            speed = 1;
+        }
+        if (gamepad1.dpad_up) { //Forward in a straight line
+            drive = 3;
+        }
+        if (gamepad1.dpad_down) { // Backward in a straight line
+            drive = 4;
+        }
+
+        // Floor-ceiling algorithm to account for motor stall torque and
+        // increase the resolution of driving
+        floorLeft = lStick1 <= 0.01 && lStick1 >= -0.01 ? 0 : 0.2 * getDirection(lStick1);
+        floorRight = rStick1 <= 0.01 && rStick1 >= -0.01 ? 0 : 0.2 * getDirection(rStick1);
+
+        // Switchcase for drivetrain direction and power. Floor-Ceiling algorithms
+        // have been included for greater control
+        switch (drive) {
+            case 1:
+                // forward default
+                rightPower = ((-lStick1 * Math.abs(lStick1)) / speed) - floorLeft;
+                leftPower = ((-rStick1 * Math.abs(rStick1)) / speed) - floorRight;
+                drive = 1;
+                break;
+            case 2:
+                // backward
+                rightPower = ((rStick1 * Math.abs(rStick1)) / speed) + floorRight;
+                leftPower = ((lStick1 * Math.abs(lStick1)) / speed) + floorLeft;
+                drive = 2;
+                break;
+            case 3:
+                rightPower = (Math.abs(rStick1) * -1) - floorRight;
+                leftPower = (Math.abs(rStick1) * -1) - floorRight;
+                drive = 1;
+                break;
+            case 4:
+                rightPower = Math.abs(rStick1) + floorRight;
+                leftPower = Math.abs(rStick1) + floorRight;
+                drive = 1;
+                break;
+
+            default:
+                leftPower = ((lStick1 * Math.abs(lStick1)) / speed) + floorLeft;
+                rightPower = ((rStick1 * Math.abs(rStick1)) / speed) + floorRight;
+        }
+
+
+        // Clips off the power in case it's over 1 or under -1 from the floor-ceiling function.
+        leftPower = Range.clip(leftPower, -1, 1);
+        rightPower = Range.clip(rightPower, -1, 1);
+
+        lfDriveM.setPower(leftPower);
+        lbDriveM.setPower(leftPower);
+        rfDriveM.setPower(rightPower);
+        rbDriveM.setPower(rightPower);
+    }
+}
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/caluper_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/caluper_teleop.java
@@ -16,8 +16,8 @@ import com.qualcomm.robotcore.util.Range;
 
 public class caluper_teleop extends OpMode {
 
-    public DcMotor lfDrive, rfDrive, lbDrive, rbDrive, liftMotor;  //Left front drive, right front drive, left back drive, right back drive.
-    public Servo lServoArm, rServoArm, jewelArm;            //lServoArm in port 5, rServoArm in port 4
+    public DcMotor lfDriveM, rfDriveM, lbDriveM, rbDriveM, liftM;  //Left front drive, right front drive, left back drive, right back drive.
+    public Servo lArmS, rArmS, jewelArmS;            //lServoArm in port 5, rServoArm in port 4
 
     private double lServoArmInit = .73;                     //Glyph arms will initialize in the open position.
     private double rServoArmInit = .1;
@@ -26,7 +26,11 @@ public class caluper_teleop extends OpMode {
     private double lServoArmAlmostGrasp = .50;
     private double rServoArmAlmostGrasp = .43;
 
-    boolean rightForwardBrake = false;                      //This variable is key in initiating an abrupt stop (see below).
+    boolean rightBackwardBrake = false;                      //These four variables initiate an abrupt stop (see below).
+    boolean rightForwardBrake = false;
+    boolean leftBackwardBrake = false;
+    boolean leftForwardBrake = false;
+
 
     private double speedFactor = .5;
     private int controlMode = 1;
@@ -34,29 +38,29 @@ public class caluper_teleop extends OpMode {
     ElapsedTime timer = new ElapsedTime();
 
     public void init () {
-        lfDrive = hardwareMap.dcMotor.get("lfDrive");       //Left front drive, Hub 1, port 2
-        lfDrive.setPower(0);
+        lfDriveM = hardwareMap.dcMotor.get("lfDriveM");       //Left front drive, Hub 1, port 2
+        lfDriveM.setPower(0);
 
-        lbDrive = hardwareMap.dcMotor.get("lbDrive");       //Left back drive, Hub 1, port 3
-        lbDrive.setPower(0);
+        lbDriveM = hardwareMap.dcMotor.get("lbDriveM");       //Left back drive, Hub 1, port 3
+        lbDriveM.setPower(0);
 
-        rfDrive = hardwareMap.dcMotor.get("rfDrive");       //Right front drive, Hub 1, port 1
-        rfDrive.setPower(0);
+        rfDriveM = hardwareMap.dcMotor.get("rfDriveM");       //Right front drive, Hub 1, port 1
+        rfDriveM.setPower(0);
 
-        rbDrive = hardwareMap.dcMotor.get("rbDrive");       //Right back drive, Hub 1, port 0
-        rbDrive.setPower(0);
+        rbDriveM = hardwareMap.dcMotor.get("rbDriveM");       //Right back drive, Hub 1, port 0
+        rbDriveM.setPower(0);
 
-        liftMotor = hardwareMap.dcMotor.get("liftMotor");   //Lift motor, Hub 2, port 3
-        liftMotor.setPower(0);
+        liftM = hardwareMap.dcMotor.get("liftM");   //Lift motor, Hub 2, port 3
+        liftM.setPower(0);
 
-        lServoArm = hardwareMap.servo.get("lServoArm");     //Left servo arm, Hub 1, port 2
-        lServoArm.setPosition(lServoArmInit);
+        lArmS = hardwareMap.servo.get("lArmS");     //Left servo arm, Hub 1, port 2
+        lArmS.setPosition(lServoArmInit);
 
-        rServoArm = hardwareMap.servo.get("rServoArm");     //Right servo arm, Hub 2, port 1
-        rServoArm.setPosition(rServoArmInit);
+        rArmS = hardwareMap.servo.get("rArmS");     //Right servo arm, Hub 2, port 1
+        rArmS.setPosition(rServoArmInit);
 
-        jewelArm = hardwareMap.servo.get("jewelArm");       //Jewel Arm, Hub 2, port 3
-        jewelArm.setPosition(.5);
+        jewelArmS = hardwareMap.servo.get("jewelArmS");       //Jewel Arm, Hub 2, port 3
+        jewelArmS.setPosition(.5);
     }
 
     public void loop() {
@@ -71,64 +75,126 @@ public class caluper_teleop extends OpMode {
         else if(gamepad1.y) {     //Hitting the "y" button will cut the drive train's power by 50%.
             controlMode = 2;
         }
-        ///DRIVER CODE
+
+///DRIVER CODE
         switch(controlMode) {
             case 1:
-                lfDrive.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //exponential scale algorithm
-                lbDrive.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //tank drive
-                rfDrive.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
-                rbDrive.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
+                lfDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //exponential scale algorithm
+                lbDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //tank drive
+                rfDriveM.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
+                rbDriveM.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
                 break;
             case 2:
-                lfDrive.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //exponential scale algorithm
-                lbDrive.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //tank drive
-                rfDrive.setPower((gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor);
-                rbDrive.setPower((gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor);
+                lfDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //exponential scale algorithm
+                lbDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //tank drive
+                rfDriveM.setPower((gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor);
+                rbDriveM.setPower((gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor);
                 break;
             default:
-                lfDrive.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //exponential scale algorithm
-                lbDrive.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //tank drive
-                rfDrive.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
-                rbDrive.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
+                lfDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //exponential scale algorithm
+                lbDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //tank drive
+                rfDriveM.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
+                rbDriveM.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
                 break;
         }
 
-        if (gamepad1.right_stick_y > 0.01) {
+        if (gamepad1.dpad_up) {
+            gamepad1.left_stick_y = gamepad1.right_stick_y;
+        }
+
+        if (gamepad1.dpad_down) {
+            gamepad1.left_stick_y = gamepad1.right_stick_y;
+        }
+
+        //ABRUPT STOP (Right Side)
+        //Backwards
+        if (gamepad1.right_stick_y > 0.01) {        //As soon as the joystick activates, set the brake variable to true.
+            rightBackwardBrake = true;
+        }
+
+        if (gamepad1.right_stick_y == 0 && rightBackwardBrake == true) {
+            timer.reset();
+            rfDriveM.setPower(-.3);          //If the brake variable is true and the joystick resets to 0, the robot will reverse power briefly to cause an abrupt stop.
+            rbDriveM.setPower(-.3);
+
+            if (timer.milliseconds() > 150) {
+                rfDriveM.setPower(0);
+                rbDriveM.setPower(0);
+            }
+            rightBackwardBrake = false;     //As soon as the robot stops, reset the brake variable to false.
+        }
+
+        //Forwards
+        if (gamepad1.right_stick_y < -0.01) {
             rightForwardBrake = true;
         }
 
         if (gamepad1.right_stick_y == 0 && rightForwardBrake == true) {
             timer.reset();
-            rfDrive.setPower(-.3);
-            rbDrive.setPower(-.3);
+            rfDriveM.setPower(.3);
+            rbDriveM.setPower(.3);
 
             if (timer.milliseconds() > 150) {
-                rfDrive.setPower(0);
-                rbDrive.setPower(0);
+                rfDriveM.setPower(0);
+                rbDriveM.setPower(0);
             }
             rightForwardBrake = false;
         }
 
+        //ABRUPT STOP (Left Side)
+        //Backwards
+        if (gamepad1.left_stick_y < -0.01) {
+            leftBackwardBrake = true;
+        }
 
-        ///OPERATOR CODE
+        if (gamepad1.left_stick_y == 0 && leftBackwardBrake == true) {
+            timer.reset();
+            lfDriveM.setPower(.3);
+            lbDriveM.setPower(.3);
+
+            if (timer.milliseconds() > 150) {
+                lfDriveM.setPower(0);
+                lbDriveM.setPower(0);
+            }
+            leftBackwardBrake = false;
+        }
+
+        //Forwards
+        if (gamepad1.left_stick_y > 0.01) {
+            leftForwardBrake = true;
+        }
+
+        if (gamepad1.left_stick_y == 0 && leftForwardBrake == true) {
+            timer.reset();
+            lfDriveM.setPower(-.3);
+            lbDriveM.setPower(-.3);
+
+            if (timer.milliseconds() > 150) {
+                lfDriveM.setPower(0);
+                lbDriveM.setPower(0);
+            }
+            leftForwardBrake = false;
+        }
+
+///OPERATOR CODE
 
         if (gamepad2.dpad_up) {
-            liftMotor.setPower(1);
+            liftM.setPower(1);
         }
         else if (gamepad2.dpad_down) {
-            liftMotor.setPower(-1);
+            liftM.setPower(-1);
         }
         else {
-            liftMotor.setPower(0);
+            liftM.setPower(0);
         }
         if (gamepad2.dpad_up) {
-            liftMotor.setPower(1);
+            liftM.setPower(1);
         }
         else if (gamepad2.dpad_down) {
-            liftMotor.setPower(-1);
+            liftM.setPower(-1);
         }
         else {
-            liftMotor.setPower(0);
+            liftM.setPower(0);
         }
 
         /*
@@ -143,12 +209,12 @@ public class caluper_teleop extends OpMode {
         */
 
         if (gamepad2.b) { //hitting the "b" button on Gamepad 2 will cause the glypher servos to grasp the glyph
-            lServoArm.setPosition(lServoArmGrasp);
-            rServoArm.setPosition(rServoArmGrasp);
+            lArmS.setPosition(lServoArmGrasp);
+            rArmS.setPosition(rServoArmGrasp);
         }
         if (gamepad2.x) { //hitting the "x" button on Gamepad 2 will cause the glypher servos to return to their original position
-            lServoArm.setPosition(lServoArmInit);
-            rServoArm.setPosition(rServoArmInit);
+            lArmS.setPosition(lServoArmInit);
+            rArmS.setPosition(rServoArmInit);
         }
 
         /*
@@ -159,22 +225,22 @@ public class caluper_teleop extends OpMode {
         */
 
         if (gamepad2.y) {   //hitting the "y" button on Gamepad 2 will cause the glypher servos to expand slightly larger than grasping the glyphs.
-            lServoArm.setPosition(lServoArmAlmostGrasp);
-            rServoArm.setPosition(rServoArmAlmostGrasp);
+            lArmS.setPosition(lServoArmAlmostGrasp);
+            rArmS.setPosition(rServoArmAlmostGrasp);
         }
 
-        /*if (gamepad2.y) {
-            jewelArm.setPosition(1);
-        }
-        else {                                  //Used only for testing.
-            jewelArm.setPosition(.5);
-        }
-        if (gamepad2.a) {
-            jewelArm.setPosition(0);
+        if (gamepad2.dpad_right) {
+            jewelArmS.setPosition(1);
         }
         else {
-            jewelArm.setPosition(.5);
+            jewelArmS.setPosition(.5);
         }
-        */
+        if (gamepad2.dpad_left) {
+            jewelArmS.setPosition(0);
+        }
+        else {
+            jewelArmS.setPosition(.5);
+        }
+
     }
 }
