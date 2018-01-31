b12586ff3f7036f2b00a18e096990aab55813d53
iRoboRaider316 <roboianhubble@gmail.com>
2018-01-30 19:16:39 -0600

    Legacy Teleop v0.3 & Legacy Auto v0.5.3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy_teleop.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,36 +6,27 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
-@TeleOp(name="legacy_teleop", group="TeleOp")
-//@Disabled
-public class legacy_teleop extends OpMode{
-
-    public DcMotor lfDriveM, lbDriveM, rfDriveM, rbDriveM, glyphLiftM, relicRotateM, relicExtendM;
-    public Servo lGlyphS, rGlyphS, jewelExtendS, jewelKnockS, relicArmS, relicMoldS;
+/**
+ * Created by jrahm on 6/14/17.
+ */
+@TeleOp (name="legacy_teleop",group="Opmode")
+public class legacy_teleop extends OpMode {
+    public DcMotor lfDriveM, rfDriveM, lbDriveM, rbDriveM, liftM;  //Left front drive, right front drive, left back drive, right back drive.
+    public Servo lArmS, rArmS;
     public CRServo glyphSlideS;
-
-    //public VoltageSensor voltage;         // for later uses
-
-    int power = 1;
-
-    boolean halfSpeed = false;
-    boolean fullSpeed = false;
-
-    double floorLeft;
-    double floorRight;
-
-    String relicArmToggle = "inOn";
-    String relicMoldToggle = "inOn";
-
-    private double lGlyphSInit = .39;               //Glyph arms will initialize in the open position.
-    private double rGlyphSInit = .61;
-    private double lGlyphSGrasp = 0;              //After testing, these positions were optimal for grasping the glyphs.
-    private double rGlyphSGrasp = 1;
-    private double lGlyphSAlmostGrasp = .2;
-    private double rGlyphSAlmostGrasp = .8;
+    //lServoArm in port 5, rServoArm in port 4
+    private double lServoArmInit = 0.5;                     //Glyph arms will initialize in the open position./
+    private double rServoArmInit = 0.5;
+    private double lServoArmGrasp = 0;                    //After testing, these positions were optimal for grasping the glyphs.
+    private double rServoArmGrasp = 1;
+    private double lServoArmAlmostGrasp = .25;
+    private double rServoArmAlmostGrasp = .75;
+    private double speedFactor = .65;
+    private int controlMode = 1;
+    private double floorLeft;
+    private double floorRight;
 
     private double getDirection(double inputPower) {
         if(inputPower == 0) {
@@ -46,212 +36,73 @@ public class legacy_teleop extends OpMode{
         }
     }
 
-    int drive = 0;
-
-    public void init() {
-        //Drive motors
-        lfDriveM = hardwareMap.dcMotor.get("lfDriveM");
+    public void init () {
+        lfDriveM = hardwareMap.dcMotor.get("lfDriveM");       //Left front drive, Hub 1, port 2
         lfDriveM.setPower(0);
-        lbDriveM = hardwareMap.dcMotor.get("lbDriveM");
+        lbDriveM = hardwareMap.dcMotor.get("lbDriveM");       //Left back drive, Hub 1, port 3
         lbDriveM.setPower(0);
-        rfDriveM = hardwareMap.dcMotor.get("rfDriveM");
+        rfDriveM = hardwareMap.dcMotor.get("rfDriveM");       //Right front drive, Hub 1, port 1
         rfDriveM.setPower(0);
-        rbDriveM = hardwareMap.dcMotor.get("rbDriveM");
+        rbDriveM = hardwareMap.dcMotor.get("rbDriveM");       //Right back drive, Hub 1, port 0
         rbDriveM.setPower(0);
-
-        //Lifting glypher motor
-        glyphLiftM = hardwareMap.dcMotor.get("glyphLiftM");
-        glyphLiftM.setPower(0);
-
-        //Glypher left-to-right servo
+        liftM = hardwareMap.dcMotor.get("liftM");   //Lift motor, Hub 2, port 3
+        liftM.setPower(0);
+        lArmS = hardwareMap.servo.get("lArmS");     //Left servo arm, Hub 1, port 2
+        lArmS.setPosition(lServoArmInit);
+        rArmS = hardwareMap.servo.get("rArmS");     //Right servo arm, Hub 2, port 1
+        rArmS.setPosition(rServoArmInit);
         glyphSlideS = hardwareMap.crservo.get("glyphSlideS");
         glyphSlideS.setPower(0);
-
-        // Relic motors
-        relicRotateM = hardwareMap.dcMotor.get("relicRotateM");
-        relicRotateM.setPower(0);
-        relicExtendM = hardwareMap.dcMotor.get("relicExtendM");
-        relicExtendM.setPower(0);
-
-        // Glyph Arms
-        lGlyphS = hardwareMap.servo.get("lGlyphS");
-        lGlyphS.setPosition(lGlyphSInit);
-        rGlyphS = hardwareMap.servo.get("rGlyphS");
-        rGlyphS.setPosition(rGlyphSInit);
-
-        // Relic Grabber Servos
-        relicArmS = hardwareMap.servo.get("relicArmS");
-        relicMoldS = hardwareMap.servo.get("relicMoldS");
-
-//        jewelExtendS = hardwareMap.servo.get("jewelExtendS");
-//        jewelKnockS = hardwareMap.servo.get("jewelKnockS");
-
-
-        lfDriveM.setDirection(DcMotor.Direction.REVERSE);       //Reverse the right side of the drive train for intuitive human interface
-        lbDriveM.setDirection(DcMotor.Direction.REVERSE);
+        lfDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lbDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rfDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rbDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lfDriveM.setDirection(DcMotorSimple.Direction.REVERSE);
+        lbDriveM.setDirection(DcMotorSimple.Direction.REVERSE);
     }
-
     public void loop() {
-        float rStick1 = gamepad1.right_stick_y;     //Simplify names in the code.
-        float lStick1 = gamepad1.left_stick_y;
-
-        boolean lBumper1 = gamepad1.left_bumper;
-        boolean rBumper1 = gamepad1.right_bumper;
-
         double leftPower;       //Used for switch-case (see below).
         double rightPower;
-        double speed;
 
-        ///OPERATOR CODE
-
-        if (gamepad2.dpad_up) {            //Hit up on the d-pad to lift the glypher.
-            glyphLiftM.setPower(1);
-        } else if (gamepad2.dpad_down) {   //Hit down on the d-pad to lower the glypher.
-            glyphLiftM.setPower(-1);
-        } else {                           //If neither button is pressed, stop the motor.
-            glyphLiftM.setPower(0);
-        }
-
-        if (gamepad2.left_trigger > 0.1) {     //If left trigger is pushed, set power to full forward.
-            glyphSlideS.setDirection(DcMotorSimple.Direction.FORWARD);
-            glyphSlideS.setPower(1);
-        } else if (gamepad2.right_trigger > 0.1) {  //If right trigger is pushed, set power to full forward.0
-            glyphSlideS.setDirection(DcMotorSimple.Direction.REVERSE);
-            glyphSlideS.setPower(1);
+        if(gamepad1.right_bumper) {
+            speedFactor = 1;
         } else {
-            glyphSlideS.setPower(0);            //Otherwise, set power to 0 (stationary).
+            speedFactor = .65;
         }
-
-        if (gamepad2.b) { //hitting the "b" button on Gamepad 2 will cause the glypher servos to grasp the glyph
-            lGlyphS.setPosition(lGlyphSGrasp);
-            rGlyphS.setPosition(rGlyphSGrasp);
+        if(gamepad1.a) {            //Hitting the "a" button will allow the drive train to run at full capacity.
+            controlMode = 1;
         }
-        if (gamepad2.x) { //hitting the "x" button on Gamepad 2 will cause the glypher servos to return to their original position
-            lGlyphS.setPosition(lGlyphSInit);
-            rGlyphS.setPosition(rGlyphSInit);
-        }
-
-        if (gamepad2.y) {   //hitting the "y" button on Gamepad 2 will cause the glypher servos to expand slightly larger than grasping the glyphs.
-            lGlyphS.setPosition(lGlyphSAlmostGrasp);
-            rGlyphS.setPosition(rGlyphSAlmostGrasp);
-        }
-
-        relicRotateM.setPower(gamepad2.left_stick_x);
-        relicExtendM.setPower(gamepad2.left_stick_y);
-
-        // Relic Arm Toggle Case
-        if (gamepad2.left_bumper && relicArmToggle == "inOn") {                // Once B is held...
-            relicArmToggle = "inOff";
-        } else if (relicArmToggle == "inOff" && !gamepad2.left_bumper) {       // Once B is released...
-            relicArmS.setPosition(1);                                          // Open Relic Arm
-            relicArmToggle = "outOn";
-        } else if (gamepad2.left_bumper && relicArmToggle == "outOn") {        // Once B is held...
-            relicArmToggle = "outOff";
-        } else if (relicArmToggle == "outOff" && !gamepad2.left_bumper) {      // Once B is released...
-            relicArmS.setPosition(0);                                          // Retract Relic Arm
-            relicArmToggle = "inOn";
-        }
-
-        // Relic Mold Thingy Toggle Case (idk what it's called)
-        if (gamepad2.right_bumper && relicMoldToggle == "inOn") {              // Once Y is held...
-            relicMoldToggle = "inOff";
-        } else if (relicMoldToggle == "inOff" && !gamepad2.right_bumper) {     // Once Y is released...
-            relicMoldS.setPosition(1);                                         // Grab the Relic!
-            relicMoldToggle = "outOn";
-        } else if (gamepad2.right_bumper && relicMoldToggle == "outOn") {      // Once Y is held...
-            relicMoldToggle = "outOff";
-        } else if (relicMoldToggle == "outOff" && !gamepad2.right_bumper) {    // Once Y is released...
-            relicMoldS.setPosition(0);                                         // Release the Relic!(55 points! YEAH! Wait, what? We didn't grab it??? sighhh...)
-            relicMoldToggle = "inOn";
-        }
-
-        ///DRIVER CODE\\\
-
-        // Universal drive train power switch case
-        if (rBumper1 && power == 1) {
-            halfSpeed = true;
-        } else if (halfSpeed && !rBumper1) {
-            power = 2;
-            halfSpeed = false;
-        } else if (rBumper1 && power == 2) {
-            fullSpeed = true;
-        } else if (fullSpeed && !rBumper1) {
-            power = 1;
-            fullSpeed = false;
-        }
-
-        switch (power) {
-            case 1:
-                // divide all powers by 1 (full speed)
-                speed = 1;
-                power = 1;
-                break;
-            case 2:
-                // divide all powers by 2 (half speed)
-                speed = 2;
-                power = 2;
-                break;
-            default:
-                speed = 1;
-                break;
-        }
-
-        // This sets the power of the drive motors to based on the joystick position using an Exponential Scale Algorithm
-        if (gamepad1.a) { // Backward 100%
-            drive = 1;
-        }
-        if (gamepad1.y) { //Forward 100%
-            drive = 2;
-        }
-        if (rBumper1) { //1/2 speed
-            speed = 2;
-        }
-        if (lBumper1) { //full speed
-            speed = 1;
-        }
-        if (gamepad1.dpad_up) { //Forward in a straight line
-            drive = 3;
-        }
-        if (gamepad1.dpad_down) { // Backward in a straight line
-            drive = 4;
+        else if(gamepad1.y) {     //Hitting the "y" button will cut the drive train's power by 50%.
+            controlMode = 2;
         }
 
         // Floor-ceiling algorithm to account for motor stall torque and
         // increase the resolution of driving
-        floorLeft = lStick1 <= 0.01 && lStick1 >= -0.01 ? 0 : 0.2 * getDirection(lStick1);
-        floorRight = rStick1 <= 0.01 && rStick1 >= -0.01 ? 0 : 0.2 * getDirection(rStick1);
+        floorLeft = gamepad1.left_stick_y <= 0.01 && gamepad1.left_stick_y >= -0.01 ? 0 : 0.2 * getDirection(gamepad1.left_stick_y);
+        floorRight = gamepad1.right_stick_y <= 0.01 && gamepad1.right_stick_y >= -0.01 ? 0 : 0.2 * getDirection(gamepad1.right_stick_y);
 
-        // Switchcase for drivetrain direction and power. Floor-Ceiling algorithms
-        // have been included for greater control
-        switch (drive) {
+///DRIVER CODE
+        switch(controlMode) {
             case 1:
-                // forward default
-                rightPower = ((-rStick1 * Math.abs(rStick1)) / speed) - floorRight;
-                leftPower = ((-lStick1 * Math.abs(lStick1)) / speed) - floorLeft;
-                drive = 1;
+                rightPower = ((-gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor) - floorLeft;
+                leftPower = ((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor) - floorRight;
                 break;
             case 2:
-                // backward
-                rightPower = ((rStick1 * Math.abs(rStick1)) / speed) + floorRight;
-                leftPower = ((lStick1 * Math.abs(lStick1)) / speed) + floorLeft;
-                drive = 2;
+                rightPower = ((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor) + floorLeft;
+                leftPower = ((gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor) + floorRight;
                 break;
-            case 3:
-                rightPower = ((rStick1 * Math.abs(rStick1)) * -1) - floorRight;
-                leftPower = ((rStick1 * Math.abs(rStick1)) * -1) - floorRight;
-                drive = 1;
-                break;
-            case 4:
-                rightPower = (rStick1 * Math.abs(rStick1)) + floorRight;
-                leftPower = (rStick1 * Math.abs(rStick1)) + floorRight;
-                drive = 1;
-                break;
-
             default:
-                leftPower = ((lStick1 * Math.abs(lStick1)) / speed) + floorLeft;
-                rightPower = ((rStick1 * Math.abs(rStick1)) / speed) + floorRight;
+                rightPower = ((-gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor) - floorLeft;
+                leftPower = ((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor) - floorRight;
+                break;
         }
-
+        if (gamepad1.dpad_up) {
+            leftPower = rightPower;
+        }
+        if (gamepad1.dpad_down) {
+            leftPower = rightPower;
+        }
 
         // Clips off the power in case it's over 1 or under -1 from the floor-ceiling function.
         leftPower = Range.clip(leftPower, -1, 1);
@@ -261,5 +112,43 @@ public class legacy_teleop extends OpMode{
         lbDriveM.setPower(leftPower);
         rfDriveM.setPower(rightPower);
         rbDriveM.setPower(rightPower);
+
+///OPERATOR CODE
+        if (gamepad2.dpad_up) {
+            liftM.setPower(1);
+        } else if (gamepad2.dpad_down) {
+            liftM.setPower(-1);
+        } else {
+            liftM.setPower(0);
+        }
+
+        if (gamepad2.b) { //hitting the "b" button on Gamepad 2 will cause the glypher servos to grasp the glyph
+            lArmS.setPosition(lServoArmGrasp);
+            rArmS.setPosition(rServoArmGrasp);
+        }
+        if (gamepad2.x) { //hitting the "x" button on Gamepad 2 will cause the glypher servos to return to their original position
+            lArmS.setPosition(lServoArmInit);
+            rArmS.setPosition(rServoArmInit);
+        }
+        if (gamepad2.y) {   //hitting the "y" button on Gamepad 2 will cause the glypher servos to expand slightly larger than grasping the glyphs.
+            lArmS.setPosition(lServoArmAlmostGrasp);
+            rArmS.setPosition(rServoArmAlmostGrasp);
+        }
+
+        if (gamepad1.left_trigger > 0.1) {     //If left trigger is pushed, set power to full forward.
+            glyphSlideS.setDirection(DcMotorSimple.Direction.FORWARD);
+            glyphSlideS.setPower(1);
+        } else if (gamepad1.right_trigger > 0.1) {  //If right trigger is pushed, set power to full forward.0
+            glyphSlideS.setDirection(DcMotorSimple.Direction.REVERSE);
+            glyphSlideS.setPower(1);
+        } else if (gamepad2.right_trigger > 0.1) {  //If right trigger is pushed, set power to full forward.0
+            glyphSlideS.setDirection(DcMotorSimple.Direction.REVERSE);
+            glyphSlideS.setPower(1);
+        } else if (gamepad2.left_trigger > 0.1) {  //If left trigger is pushed, set power to full forward.0
+            glyphSlideS.setDirection(DcMotorSimple.Direction.FORWARD);
+            glyphSlideS.setPower(1);
+        } else {
+            glyphSlideS.setPower(0);            //Otherwise, set power to 0 (stationary).
+        }
     }
 }
