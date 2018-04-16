473509fb15ae747666558d9740a831cf5797f0d1
Jason Rahm <citizenelah@gmail.com>
2018-04-16 17:13:52 -0500

    Updated lift controls to include staging. Added aesthetics to code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy_teleop.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,44 +10,61 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+
 /**
- * Created by A horde of cheezy puffs TM on 6/14/17.
+ * Created by Jake from Robo Raiders FTC 7129 on 6/14/17.
  */
+
 @TeleOp (name="legacy_teleop",group="Opmode")
+
 public class legacy_teleop extends OpMode {
+
     public DcMotor lfDriveM, rfDriveM, lbDriveM, rbDriveM, glyphLiftM, relicTurn, relicExtend;  //Left front drive, right front drive, left back drive, right back drive.
     public Servo lGlyphS, rGlyphS, jewelExtendS, jewelHitS, relicGrabber, relicLower;
     public CRServo glyphSlideS;
+
     DigitalChannel sensorDigitalMagnetic;
     ElapsedTime runtime = new ElapsedTime();
-    private double lServoArmInit = 0.65;                     //Glyph arms will initialize in the open position./
+
+    int liftCounter; //liftCounter and resetJoystick are used for lift controls.
+    boolean resetJoystick = false;
+
+    private double lServoArmInit = 0.65; //Glyph arms will initialize in the open position.
     private double rServoArmInit = 0.4;
-    private double lServoArmGrasp = 0;                    //After testing, these positions were optimal for grasping the glyphs.
+    private double lServoArmGrasp = 0; //Variables for glyph
     private double rServoArmGrasp = 1;
     private double lServoArmAlmostGrasp = 0.5;
     private double rServoArmAlmostGrasp = 0.6;
+
     private double relicGrab = 1;
     private double relicRelease = 0;
     private double relicDown = 0.9;
     private double relicRaise = 0.3;
     private double relicSlow1 = 0.2;
     private double relicSlow2 = 0.35;
+
     private double speedFactor = .7;
     private int controlMode = 1;
     private String relicToggle = "inOn";
     private String relicArmToggle = "inOn";
+
     private double pExtendArm = .3; //Partially extend the jewel arm.
     private double extendArm = .55; //Fully extend the jewel arm.
     private double retractArm = 0; //Bring jewel arm back to the robot.
+
     private double hitCenter = .5;   //jewelHitS returns to center variable
     private double hitLeft = 0;      //jewelHitS Hit left jewel variable
     private double hitPLeft = .25;   //Protects arm from falling in case of power outages.
     private double hitRight = 1;     //jewelHitS Hits right jewel variable
+
     boolean first = false;
+
     enum SlippyState {
         Idle, LookingMagnet, FoundMagnet                    //These are the different states that the
     }
+
     public void init () {
+
         lfDriveM = hardwareMap.dcMotor.get("lfDriveM");       //Left front drive, Hub 1, port 2
         lfDriveM.setPower(0);
         lbDriveM = hardwareMap.dcMotor.get("lbDriveM");       //Left back drive, Hub 1, port 3
@@ -54,35 +72,46 @@ public class legacy_teleop extends OpMode {
         rfDriveM = hardwareMap.dcMotor.get("rfDriveM");       //Right front drive, Hub 1, port 1
         rfDriveM.setPower(0);
         rbDriveM = hardwareMap.dcMotor.get("rbDriveM");       //Right back drive, Hub 1, port 0
-
         rbDriveM.setPower(0);
+
         glyphLiftM = hardwareMap.dcMotor.get("glyphLiftM");   //Lift motor, Hub 2, port 3
         glyphLiftM.setPower(0);
+
         lGlyphS = hardwareMap.servo.get("lGlyphS");     //Left servo arm, Hub 1, port 2
         lGlyphS.setPosition(lServoArmInit);
         rGlyphS = hardwareMap.servo.get("rGlyphS");     //Right servo arm, Hub 2, port 1
         rGlyphS.setPosition(rServoArmInit);
+
         glyphSlideS = hardwareMap.crservo.get("glyphSlideS");
         glyphSlideS.setPower(0);
+
         jewelExtendS = hardwareMap.servo.get("jewelExtendS");   //Hub 3 Servo 0
         jewelExtendS.setPosition(retractArm);
         jewelHitS = hardwareMap.servo.get("jewelHitS");     //Hub 2 Servo 4
+
         relicTurn = hardwareMap.dcMotor.get("relicTurnM");       //Relic Turn Motor, Hub 2, Port 0
         relicTurn.setPower(0);
         relicExtend = hardwareMap.dcMotor.get("relicExtendM");   //Relic Extend Motor, Hub 2, port 1 20
         relicExtend.setPower(0);
         relicGrabber = hardwareMap.servo.get("relicGrabberS");
         relicLower = hardwareMap.servo.get("relicLiftS");
+
         relicTurn.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         relicExtend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         lfDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lbDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rfDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rbDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         glyphLiftM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        glyphLiftM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        glyphLiftM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         sensorDigitalMagnetic = hardwareMap.get(DigitalChannel.class, "SensorDigitalMagnetic");
         sensorDigitalMagnetic.setMode(DigitalChannel.Mode.INPUT);
     }
+
     private SlippyState slippystate = SlippyState.Idle;
 
     @Override
@@ -97,59 +126,49 @@ public class legacy_teleop extends OpMode {
 
     public void loop() {
         if(gamepad1.right_bumper) {
-            speedFactor = 1;
+            speedFactor = 1; //When the right bumper is held down, run the drive train at full capacity.
         } else {
-            speedFactor = .65;
+            speedFactor = .65; //Otherwise, run at 65% power.
         }
-        if(gamepad1.a) {            //Hitting the "a" button will allow the drive train to run at full capacity.
+        if(gamepad1.a) {    //Either button will dictate the drive train's control mode, shown in the switch case directly below.
             controlMode = 1;
         }
-        else if(gamepad1.y) {     //Hitting the "y" button will cut the drive train's power by 50%.
+        else if(gamepad1.y) {
             controlMode = 2;
         }
-///DRIVER CODE
+
+///----------------------------------------DRIVER CODE----------------------------------------\\\
+
+        ///DRIVE TRAIN CONTROLS
+
         switch(controlMode) {
-            case 1:
-                lfDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //exponential scale algorithm
-                lbDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //tank drive
+            default: case 1: //Normal Tank Drive
+                lfDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //Math.abs function allows for exponential scale drive.
+                lbDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //Less input gives more control, more input gives more power.
                 rfDriveM.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
                 rbDriveM.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
                 break;
-            case 2:
-                lfDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //exponential scale algorithm
-                lbDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //tank drive
+            case 2: //
+                lfDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor);
+                lbDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor);
                 rfDriveM.setPower((gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor);
                 rbDriveM.setPower((gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor);
                 break;
-            default:
-                lfDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //exponential scale algorithm
-                lbDriveM.setPower((-gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) * speedFactor); //tank drive
-                rfDriveM.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
-                rbDriveM.setPower((gamepad1.right_stick_y * Math.abs(gamepad1.right_stick_y)) * speedFactor);
-                break;
         }
-        if (gamepad1.dpad_up || gamepad1.dpad_down) {
+        if (gamepad1.dpad_up || gamepad1.dpad_down) { //If either up or down is pressed on gamepad 1, set both sides of the drive train to the right joystick.
             gamepad1.left_stick_y = gamepad1.right_stick_y;
         }
-///OPERATOR CODE
+
+///---------------------------------------OPERATOR CODE---------------------------------------\\\
+
+        ///RELIC CONTROLS
+
         relicExtend.setPower(-gamepad2.right_stick_y);
         relicTurn.setPower(-gamepad2.left_stick_x/5);
         if (gamepad2.left_stick_x == 0 && !first) {
             relicTurn.setPower(-0.1);
             first = true;
         }
-//        if (gamepad2.left_bumper) {
-//            relicLower.setPosition(relicRaise);
-//        }
-//        else if (gamepad2.right_bumper) {
-//            relicLower.setPosition(relicDown);
-//        }
-//        if (gamepad2.y) {
-//            relicGrabber.setPosition(relicGrab);
-//        }
-//        else if (gamepad2.a) {
-//            relicGrabber.setPosition(relicRelease);
-//        }
         if (gamepad2.dpad_right) {
             relicGrabber.setPosition(relicSlow1);
         } else if (gamepad2.dpad_left) {
@@ -185,7 +204,18 @@ public class legacy_teleop extends OpMode {
             relicArmToggle = "inOn";
             relicLower.setPosition(relicRaise);
         }
-
+        /*if (gamepad2.left_bumper) {
+            relicLower.setPosition(relicRaise);
+        }
+        else if (gamepad2.right_bumper) {
+            relicLower.setPosition(relicDown);
+        }
+        if (gamepad2.y) {
+            relicGrabber.setPosition(relicGrab);
+        }
+        else if (gamepad2.a) {
+            relicGrabber.setPosition(relicRelease);
+        }*/
         if (gamepad2.dpad_up) {
             if(relicLower.getPosition() == relicDown && relicDown > 0) {
                 relicDown -= 0.01;
@@ -204,13 +234,84 @@ public class legacy_teleop extends OpMode {
             glyphLiftM.setPower(0);
         }
 
-        if (gamepad2.dpad_up) {
-            glyphLiftM.setPower(1);
-        } else if (gamepad2.dpad_down) {
-            glyphLiftM.setPower(-1);
-        } else {
-            glyphLiftM.setPower(0);
+        //LIFT CONTROLS
+
+        if (gamepad1.left_stick_y < -0.01 && !resetJoystick) {
+            liftCounter += 1;
+            if (liftCounter > 4) {
+                liftCounter = 4;
+            }
+            resetJoystick = true;
+        } else if (gamepad1.left_stick_y > 0.01 && !resetJoystick) {
+            liftCounter += -1;
+            if (liftCounter < 0) {
+                liftCounter = 0;
+            }
+            resetJoystick = true;
         }
+        if (gamepad1.left_stick_y == 0 && resetJoystick) {
+            resetJoystick = false;
+        }
+
+        switch (liftCounter) {
+            case 0:
+                glyphLiftM.setTargetPosition(0);
+                glyphLiftM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                if (glyphLiftM.isBusy()) {
+                    glyphLiftM.setPower(1);
+                    telemetry.addData("Current Position", glyphLiftM.getCurrentPosition());
+                    telemetry.update();
+                } else {
+                    glyphLiftM.setPower(0);
+                }
+                break;
+            case 1:
+                glyphLiftM.setTargetPosition(300);
+                glyphLiftM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                if (glyphLiftM.isBusy()) {
+                    glyphLiftM.setPower(1);
+                    telemetry.addData("Current Position", glyphLiftM.getCurrentPosition());
+                    telemetry.update();
+                } else {
+                    glyphLiftM.setPower(0);
+                }
+                break;
+            case 2:
+                glyphLiftM.setTargetPosition(1300);
+                glyphLiftM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                if (glyphLiftM.isBusy()) {
+                    glyphLiftM.setPower(1);
+                    telemetry.addData("Current Position", glyphLiftM.getCurrentPosition());
+                    telemetry.update();
+                } else {
+                    glyphLiftM.setPower(0);
+                }
+                break;
+            case 3:
+                glyphLiftM.setTargetPosition(2300);
+                glyphLiftM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                if (glyphLiftM.isBusy()) {
+                    glyphLiftM.setPower(1);
+                    telemetry.addData("Current Position", glyphLiftM.getCurrentPosition());
+                    telemetry.update();
+                } else {
+                    glyphLiftM.setPower(0);
+                }
+                break;
+            case 4:
+                glyphLiftM.setTargetPosition(3300);
+                glyphLiftM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                if (glyphLiftM.isBusy()) {
+                    glyphLiftM.setPower(1);
+                    telemetry.addData("Current Position", glyphLiftM.getCurrentPosition());
+                    telemetry.update();
+                } else {
+                    glyphLiftM.setPower(0);
+                }
+                break;
+        }
+
+        //GLYPHER CONTROLS
 
         if (gamepad2.b) { //hitting the "b" button on Gamepad 2 will cause the glypher servos to grasp the glyph
             lGlyphS.setPosition(lServoArmGrasp);
